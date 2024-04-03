a59c5b7a781f50ec1fdbae65caa224553143dcdf
NoyaAberjil <noyaaberjil@gmail.com>
2024-04-03 13:46:03 +0300

    adding pipes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import random
 
 pygame.init()
 
@@ -12,9 +13,11 @@ running = True
 screen = pygame.display.set_mode((screen_width, screen_hight))
 pygame.display.set_caption('Flappy Bird - by Noya aberjil')
 
+
 backgrund= pygame.image.load("bg.png")
 ground_scroll_pic = pygame.image.load("ground_scorll.png")
 ground_pic = pygame.image.load("ground_pic.png")
+
 
 image_sprite_flappy = [pygame.image.load("bird1.png"),
                 pygame.image.load("bird2.png"),
@@ -27,15 +30,52 @@ flappy_moving = 0
 
 x_pipe = 0
 y_pipe = 0
-image_pipe = pygame.image.load("pipe.png")
+pipe = pygame.image.load("pipe.png")
+pipe2 = pygame.image.load("pipe_2.png")
+pipe3 = pygame.image.load("pipe_3.png")
+pipe4 = pygame.image.load("pipe_4.png")
+pipe_list = [pipe, pipe2, pipe3, pipe4]
+pipes = []
+MAX_PIPES = 4
 
 
 ground_scroll = 0
-scroll_speed = 3
+scroll_speed = 1
 
 
 flappy_vel = 2
 jumping = False
+
+def check_players():
+    global pipes
+    if len(pipes)==0:
+        return True
+    elif pipes[0][1] < 330:     
+        return True
+    else:
+        return False
+        
+def generatePalyers():
+    global pipes
+    global pipe_list
+
+    if check_players() and (len(pipes) < MAX_PIPES):
+        pipe = pipe_list[random.randint(0, len(pipe_list) - 1)]
+        pipes.append([pipe, 426])
+
+def move_pipes():
+    global pipes
+    for pipe in pipes:
+        pipe[1] -= scroll_speed
+
+def show_players():
+    global screen
+    global pipes
+    global backgrund
+
+    print(pipes)
+    for pipe in pipes:
+        screen.blit(pipe[0], (pipe[1], backgrund.get_size()[1] - pipe[0].get_size()[1]))
 
 # Run until the user asks to quit
 while running:
@@ -67,6 +107,10 @@ while running:
     screen.blit(ground_pic, (0,428))
     screen.blit(ground_scroll_pic, (ground_scroll,428))
     screen.blit(bird_image, (x_flappy, y_flappy + flappy_vel))
+    move_pipes()
+    generatePalyers()
+    show_players()
+
     ground_scroll -= scroll_speed
 
     if abs(ground_scroll) > 25:
